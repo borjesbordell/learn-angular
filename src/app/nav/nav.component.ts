414afa1f7c06df9abe6f2a22fb3b6d6671d772b9
bob <borjesbordell5@gmail.com>
2018-03-01 17:31:59 +0100

    fixed menu

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,6 +12,23 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // let isOpen = true;
+
+  // menuLinkClicked() {
+  //   console.log('clicked');
+  //   if (!isOpen) {
+  //     $('#navbarColor02').addClass('open');
+  //   } else {
+  //     $('#navbarColor02').removeClass('open');
+  //   }
+  //   isOpen = !isOpen;
+  // }
+
+  menuClick(): void {
+    console.log('clicked');
+    $('#navbarColor02').collapse();
   }
 
 }
